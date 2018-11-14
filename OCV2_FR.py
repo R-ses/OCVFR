9c8b7c54ac85b5751a6cc5642b0b4123d4934df3
Ramsses <rxtechtips@gmail.com>
2018-11-13 22:16:33 -0800

    OCVFR V1.1 Small_things

--- OCV2_FR.py
+++ OCV2_FR.py
@@ -8,6 +8,7 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5, minNeighbors=5)
+    #front_face
     for (x,y,w,h) in faces:
         print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w] #[Y+heigh, X+width]
--- faces_train.py
+++ faces_train.py
@@ -1,10 +1,20 @@
 import os
-
+from PIL import Image
+import numpy as np
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
+
+y_labels = []
+x_train= []
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith("png") or file.endswith("jpg"):
            path = os.path.join(root,file)
-           print(path)
+           label = os.path.basename(root).replace(" ","_").lower()
+           print(label,path)
+           #x_train.append(path)
+           #y_labels.append(label)
+           pil_image = Image.open(path).convert("L") #grayscale
+           image_array = np.array(pil_image, "uint8")
+           print(image_array)
